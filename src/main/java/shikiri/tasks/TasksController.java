fe2e63bdb62ef30950e378f53197808da7d84763
Juliakp1 <juliakp710@gmail.com>
2024-04-08 13:09:27 -0300

    fixed variables

--- src/main/java/shikiri/tasks/TasksController.java
+++ src/main/java/shikiri/tasks/TasksController.java
@@ -3,7 +3,6 @@ package shikiri.tasks;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +13,11 @@ public interface TasksController {
 
     @PostMapping("/tasks")
     public ResponseEntity<TasksOut> create(
-        @RequestBody(required = true) String name,
         @RequestBody(required = true) TasksIn in
     );
 
     @PutMapping("/tasks/{id}")
     public ResponseEntity<TasksOut> update(
-        @PathVariable(required = true) String id,
-        @RequestBody(required = true) String user,
         @RequestBody(required = true) TasksIn in
     );
 
